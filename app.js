ceb8aaf02788b8483ada40d268871108cb8abd81
wander misael <misaelnova13@gmail.com>
2025-10-12 16:06:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-    let btnNavIcono = document.getElementById('fa-bars');
     let NAVConteinerInterno = document.getElementById('nav-container-inner');
     let cambiarIcono = document.getElementById('fa-bars')
 
 
-
-btnNavIcono.addEventListener('click', () => {
-  // Mostrar u ocultar menú
-  NAVConteinerInterno.classList.toggle('display-none');
-});
  
 const menuIcon = document.getElementById('fa-bars');
 const navMenu = document.getElementById('nav-container-inner');
 
 menuIcon.addEventListener('click', () => {
-    // Alternar menú
-    navMenu.classList.toggle('show-menu');
-    
-    // Cambiar icono de barras a X
+    // Alternar icono de barras a X
     menuIcon.classList.toggle('fa-bars');
     menuIcon.classList.toggle('fa-times');
-});
 
+    // Alternar visibilidad del menú
+    if (NAVConteinerInterno.style.display === 'block') {
+        NAVConteinerInterno.style.display = 'none';
+    } else {
+        NAVConteinerInterno.style.display = 'block';
+    }
+});
